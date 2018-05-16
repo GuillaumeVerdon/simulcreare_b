12f7e45f78787dd622dc99725b322fab8bbc323f
Guillaume Verdon <guillaume.verdon.pro@gmail.com>
2018-05-16 23:15:06 +0200

    Added user entity and repository

---
+++ src/main/java/com/simulcreare/domain/entity/User.java
@@ -0,0 +1,37 @@
+package com.simulcreare.domain.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String mail;
+    private String passhash;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPasshash() {
+        return passhash;
+    }
+
+    public void setPasshash(String passhash) {
+        this.passhash = passhash;
+    }
+}
---
+++ src/main/java/com/simulcreare/respository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.simulcreare.respository;
+
+import com.simulcreare.domain.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
