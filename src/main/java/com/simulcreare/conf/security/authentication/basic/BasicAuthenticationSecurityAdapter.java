1ec25b1a5e1ddca5db371be2c3fa73be6126baad
Guillaume Verdon <guillaume.verdon.pro@gmail.com>
2018-05-18 22:50:06 +0200

    Added csrf configuration

--- src/main/java/com/simulcreare/conf/security/authentication/basic/BasicAuthenticationSecurityAdapter.java
+++ src/main/java/com/simulcreare/conf/security/authentication/basic/BasicAuthenticationSecurityAdapter.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class BasicAuthenticationSecurityAdapter extends WebSecurityConfigurerAdapter {
@@ -28,7 +29,9 @@ public class BasicAuthenticationSecurityAdapter extends WebSecurityConfigurerAda
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
             .authorizeRequests()
             .antMatchers("/register").permitAll()
             .antMatchers("/h2/**").permitAll()
